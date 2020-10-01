522115e49ecb25528a410cb70b6f9febc08f969c
Gustavo Possebon <gustavo.possebon@acad.pucrs.br>
2020-10-01 19:26:46 -0300

    Layout parte 1 - Constraint Layouts

---
+++ Layouts Android parte 1 Criando telas com Constraint Layout/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,32 @@
+package br.com.alura.aluraviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
+
+import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.dao.PacoteDAO;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.ui.adapter.ListaPacotesAdapter;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pacotes";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+
+        setTitle(TITULO_APPBAR);
+        configuraLista();
+    }
+
+    private void configuraLista() {
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+        List<Pacote> pacotes = new PacoteDAO().lista();
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
+    }
+}
