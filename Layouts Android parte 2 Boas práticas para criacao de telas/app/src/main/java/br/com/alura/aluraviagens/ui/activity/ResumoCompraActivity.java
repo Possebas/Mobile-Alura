0d2ce279f71f8942adb3c22f817ba9e8f16403dc
Gustavo Possebon <gustavo.possebon@acad.pucrs.br>
2020-10-02 12:48:03 -0300

    Layouts Android parte 2 Boas praticas para criacao de telas

---
+++ Layouts Android parte 2 Boas práticas para criacao de telas/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -0,0 +1,74 @@
+package br.com.alura.aluraviagens.ui.activity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.DataUtil;
+import br.com.alura.aluraviagens.util.MoedaUtil;
+import br.com.alura.aluraviagens.util.ResourceUtil;
+
+import static br.com.alura.aluraviagens.ui.activity.PacoteActivityConstantes.CHAVE_PACOTE;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Resumo da compra";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+        setTitle(TITULO_APPBAR);
+        carregaPacoteRecebido();
+    }
+
+    private void carregaPacoteRecebido() {
+        Intent intent = getIntent();
+        if (intent.hasExtra(CHAVE_PACOTE)){
+            Pacote pacote = (Pacote) intent.getSerializableExtra(CHAVE_PACOTE);
+            inicializaCampos(pacote);
+        }
+    }
+
+    private void inicializaCampos(Pacote pacote) {
+        mostraLocal(pacote);
+        mostraImagem(pacote);
+        mostraData(pacote);
+        mostraPreco(pacote);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.resumo_compra_preco_pacote);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_compra_data_viagem);
+        String periodoEmTexto = DataUtil
+                .periodoEmTexto(pacote.getDias());
+        data.setText(periodoEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawableDoPacote = ResourceUtil
+                .devolveDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_compra_local_pacote);
+        local.setText(pacote.getLocal());
+    }
+}
---
+++ Layouts Android parte 2 Boas práticas para criacao de telas/app/src/main/java/br/com/alura/aluraviagens/util/DataUtil.java
@@ -0,0 +1,25 @@
+package br.com.alura.aluraviagens.util;
+
+import android.support.annotation.NonNull;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DataUtil {
+
+    public static final String DIA_E_MES = "dd/MM";
+
+    @NonNull
+    public static String periodoEmTexto(int dias) {
+        Calendar dataIda = Calendar.getInstance();
+        Calendar dataVolta = Calendar.getInstance();
+        dataVolta.add(Calendar.DATE, dias);
+        SimpleDateFormat formatoBrasileiro = new SimpleDateFormat(DIA_E_MES);
+        String dataFormatadaIda = formatoBrasileiro.format(dataIda.getTime());
+        String dataFormatadaVolta = formatoBrasileiro.format(dataVolta.getTime());
+        return dataFormatadaIda + " - "
+                + dataFormatadaVolta + " de "
+                + dataVolta.get(Calendar.YEAR);
+    }
+
+}
